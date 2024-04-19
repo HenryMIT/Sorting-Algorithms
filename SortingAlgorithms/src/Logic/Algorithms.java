5fe33f4012d99c32a103a4aefba9df17becc8dda
HenryMIT <116330681+HenryMIT@users.noreply.github.com>
2024-04-18 19:52:20 -0600

    Modify class Algorithm and TimeCalculation

--- SortingAlgorithms/src/Logic/Algorithms.java
+++ SortingAlgorithms/src/Logic/Algorithms.java
@@ -4,41 +4,58 @@
  */
 package Logic;
 
+import java.util.Random;
+
 /**
  *
  * @author Usuario
  */
 public class Algorithms {
-
+     
+    int[] listNumber=new int[100000];
+    
     public Algorithms() {
-    
+        //Fill the vector with numbers from 1 to 100000
+        fillArray();
     }
     
-    //Basic algorithm
-    
-    public void BubbleSort(int[] listNumber){
-    
-    }
-    
-    //Unstable algorithms
-    
-    public void ShellSort(int[] listNumber){
-    
-    }
-    
-    public void QuickSort (int[] listNumber){
-    
-    }
-    
-    //Stable algorithms
-    
-    public void MergeSort(){
-    
-    }
-    
-    public void RadixSort(){
-        
+    public void fillArray() {
+        Random randon = new Random();
+        for (int i = 0; i < this.listNumber.length; i++) {
+            this.listNumber[i] = randon.nextInt(100000);
+        }
     }
 
-    
+    //Basic algorithm
+    public void BubbleSort(int[] listNumber) {
+        int temp = 0;
+        for (int i = 0; i < listNumber.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (listNumber[j] < listNumber[j+1]) {
+                    temp=listNumber[j+1];
+                    listNumber[j+1]=listNumber[j];
+                    listNumber[j]=temp;
+                }
+            }
+        }
+    }
+
+    //Unstable algorithms
+    public void ShellSort(int[] listNumber) {
+
+    }
+
+    public void QuickSort(int[] listNumber) {
+
+    }
+
+    //Stable algorithms
+    public void MergeSort() {
+
+    }
+
+    public void RadixSort() {
+
+    }
+
 }
