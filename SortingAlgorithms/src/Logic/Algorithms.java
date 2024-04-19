694161e44ff926e70d0b8d154d62d5d66ae2fbaa
HenryMIT <116330681+HenryMIT@users.noreply.github.com>
2024-04-18 20:45:14 -0600

    add algorithm BubbleSort

--- SortingAlgorithms/src/Logic/Algorithms.java
+++ SortingAlgorithms/src/Logic/Algorithms.java
@@ -11,26 +11,28 @@ import java.util.Random;
  * @author Usuario
  */
 public class Algorithms {
-    
+
     //Atribute
-    int[] listNumber=new int[100000];
-    
+    int[] listNumber = new int[100000];
+
     public Algorithms() {
         //Fill the vector with numbers from 1 to 100000
         fillArray();
     }
-    
+
     public void fillArray() {
         Random randon = new Random();
         for (int i = 0; i < this.listNumber.length; i++) {
             this.listNumber[i] = randon.nextInt(100000);
         }
     }
-    
-        public void printList() {
+
+    public void printList() {
+        int n = listNumber.length;
         System.out.println("List Number {");
-        for (int i = 0; i < listNumber.length; i++) {
-            System.out.println(listNumber[i]);
+        for (int i = 0; i < n; i++) {
+            String ln = i % 20 == 0 ? ", \n" : " , ";
+            System.out.print(listNumber[i] + ln);
         }
         System.out.println("}");
     }
@@ -38,12 +40,16 @@ public class Algorithms {
     //Basic algorithm
     public void BubbleSort() {
         int temp = 0;
-        for (int i = 0; i < listNumber.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (listNumber[j] < listNumber[j+1]) {
-                    temp=listNumber[j+1];
-                    listNumber[j+1]=listNumber[j];
-                    listNumber[j]=temp;
+        int n = listNumber.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                // Compares the numbers in the array and orders them
+                if (listNumber[j] > listNumber[j + 1]) {
+                    //Exchange listNumber[j] with listNumber[j+1]
+                    temp = listNumber[j];
+                    listNumber[j] = listNumber[j + 1];
+                    listNumber[j + 1] = temp;
                 }
             }
         }
@@ -51,7 +57,20 @@ public class Algorithms {
 
     //Unstable algorithms
     public void ShellSort() {
-
+        int n = listNumber.length;
+        int gap = n / 2; // Divide the array into 2 for the first time
+        while (gap > 0) {
+            for (int i = gap; i < n; i++) {
+                int key = listNumber[i];                               
+                int j = i;
+                while (j >= gap && listNumber[j - gap] > key) {
+                    listNumber[j] = listNumber[j - gap];
+                    j -= gap;
+                }
+                listNumber[j] = key;
+            }
+            gap /= 2;//Divide the previous interval into 2
+        }
     }
 
     public void QuickSort() {
--- SortingAlgorithms/src/Logic/TimeCalculation.java
+++ SortingAlgorithms/src/Logic/TimeCalculation.java
@@ -15,33 +15,17 @@ import java.util.TimerTask;
  */
 public class TimeCalculation {
 
-    //Attributes 
-    private int arrayTest[] = new int[100000];
+    Algorithms algoritms;
 
     public TimeCalculation() {
-        //Fill the vector with numbers from 1 to 100000
-        fillArray();
-    }
-
-    public void fillArray() {
-        Random randon = new Random();
-        for (int i = 0; i < this.arrayTest.length; i++) {
-            this.arrayTest[i] = randon.nextInt(100000);
-        }
-    }
-
-    public void printList() {
-        System.out.println("List Number {");
-        for (int i = 0; i < arrayTest.length; i++) {
-            System.out.println(arrayTest[i]);
-        }
-        System.out.println("}");
+        algoritms = new Algorithms();
     }
 
     public double ms_Calculation() {
         final double MS_PER_SECUND = 1000;
         long START_MS = System.currentTimeMillis();
         long TIME_SPENT_MS = System.currentTimeMillis() - START_MS;
+        
         double realTime = (float) (TIME_SPENT_MS / MS_PER_SECUND);
 
         return realTime;
@@ -52,6 +36,7 @@ public class TimeCalculation {
         final double NS_PER_SECUND = 1000000000;
         long START_NS = System.nanoTime();
         long TIME_SPENT_NS = System.nanoTime() - START_NS;
+       // algoritms.BubbleSort();
         double realTime = (float) (TIME_SPENT_NS / NS_PER_SECUND);
 
         return realTime;
@@ -62,13 +47,14 @@ public class TimeCalculation {
         double resultMili = 0;
 
         System.out.println("We are testing at: " + new Date()
-                + "\n Algorithm name: ");// <- Name of the algorithm you are going to test
+                + "\n Algorithm name: BubbleSort");// <- Name of the algorithm you are going to test
+        algoritms.BubbleSort();
         resultMili = ms_Calculation();
         //Algorithm test in miliseconds:
-
+        algoritms.BubbleSort();
         resultNano = ns_Calculation();
         //Algorithm test in nanoseconds:
-
+        
         System.out.println("TEST: Results from the execute"
                 + "\n" + "The algorithm spent : " + resultMili + " miliSeconds"
                 + "\n" + "The algorithm spent : " + resultNano + " nanoSeconds"
