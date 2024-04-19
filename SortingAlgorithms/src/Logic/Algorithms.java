7805af1fc95dd4a065fe9a802a243740daa50289
HenryMIT <116330681+HenryMIT@users.noreply.github.com>
2024-04-18 19:56:57 -0600

    add printList() in Algorithms

--- SortingAlgorithms/src/Logic/Algorithms.java
+++ SortingAlgorithms/src/Logic/Algorithms.java
@@ -26,6 +26,14 @@ public class Algorithms {
             this.listNumber[i] = randon.nextInt(100000);
         }
     }
+    
+        public void printList() {
+        System.out.println("List Number {");
+        for (int i = 0; i < listNumber.length; i++) {
+            System.out.println(listNumber[i]);
+        }
+        System.out.println("}");
+    }
 
     //Basic algorithm
     public void BubbleSort() {
